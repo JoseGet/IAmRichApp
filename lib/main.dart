1ff6ad144275909e256d583cea5b110abcbad704
Jose Getulio <getuliojose96@gmail.com>
2024-12-07 00:15:53 -0400

    Finishing of I am Rich app, changed the type of image used(networkingImage to AssetImage) and change the image icon from Android device

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,20 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-        home: Scaffold(
-      backgroundColor: Colors.blueGrey,
-      appBar: AppBar(
-        backgroundColor: Colors.blueGrey[900],
-        title: const Text("I am Rich"),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          title: const Text("I am Rich"),
+          centerTitle: true,
+          titleTextStyle: const TextStyle(color: Colors.white, fontSize: 25),
+        ),
+        body: const Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
       ),
-      body: const Center(
-        child: Image(
-            image: NetworkImage(
-                'https://investnews.com.br/wp-content/uploads/2021/06/doge-1200x800.jpg')),
-      ),
-    )),
+    ),
   );
 }
